998189b9739fd6b8229c754ffe6e8796bb4f4298
ENTOMDT <entomdt@gmail.com>
2021-10-18 13:13:31 -0500

    Migrate Daemonium from vanilla-sky

--- src/App.js
+++ src/App.js
@@ -4,20 +4,74 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="game-board">
+
+        <div class="daemon-area">
+
+          <h1>
+            <strong>EMPYREAN</strong> <br/> Choose your response to the play of the Heavenly City...
+          </h1>
+
+          <div class="stats daemon-stats" onClick="showStats()">
+            <div class="life-bar">
+              <div class="life-left daemon-color"></div>
+            </div>
+            <div class="life-total"></div>
+            <div class="thumbnail">💦</div>
+            <div class="name m-index-name">MI</div>
+          </div>
+          
+          <div class="card daemon-card daemon-color">
+            <div class="text"></div>
+            <img class="image" alt="daemon card one" />
+            <div class="power"></div>
+          </div>
+
+        </div>
+
+        <div class="player-area">
+
+          <div class="stats player-stats" onClick="showStats()">
+            <div class="life-bar">
+              <div class="life-left player-color"></div>
+            </div>
+            <div class="life-total"></div>
+            <div class="thumbnail">🌡</div>
+            <div class="name h-index-name">HI</div>
+          </div>
+
+          <div class="card player-card player-color">
+            <div class="text"></div>
+            <img class="image" alt="player card one" />
+            <div class="power"></div>
+          </div>
+
+          <div class="card player-card player-color">
+            <div class="text"></div>
+            <img class="image" alt="player card two" />
+            <div class="power"></div>
+          </div>
+
+          <div class="card player-card player-color">
+            <div class="text"></div>
+            <img class="image" alt="player card tree" />
+            <div class="power"></div>
+          </div>
+
+          <button class="start-game" onClick="startGame()">Play the Game!</button>
+          <button class="next-turn" onClick="playTurn()">Next!</button>
+        </div>
+      </div>
+
+
+
+      <div class="winner-section">
+        <div>
+          <span class="winner-message">You have fallen...</span>
+          <button class="restart" onClick="location.reload()">Play Again</button>
+          <button onClick="showStats()">Show Stats</button>
+        </div>
+      </div>
     </div>
   );
 }
